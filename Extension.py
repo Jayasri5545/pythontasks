6351df34d3a88c7af4ad6f95fa539e96eb0daf5c
Jayasri5545 <85301474+Jayasri5545@users.noreply.github.com>
2021-06-09 19:27:11 +0530

    Update Extension.py

--- Extension.py
+++ Extension.py
@@ -1,3 +1,4 @@
-filename = input("Input the Filename: ")
-extension = filename.split(".")
+#Python program to print the extension of the filename
+file_name=input("Input the filename: ")
+extension= file_name.split(".")
 print ("The extension of the file is : ", extension[-1])
