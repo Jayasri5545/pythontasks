183efa524dde55961d6f4078a6d5e5da0291fb8d
Jayasri5545 <85301474+Jayasri5545@users.noreply.github.com>
2021-06-09 19:23:40 +0530

    Update radius.py

--- radius.py
+++ radius.py
@@ -1,4 +1,5 @@
 # Radius of a circle
+PI=3.14
 r=float(input("Input the radius of the circle:"))
-area=(22/7)*r*r
-print("The area of the circle with radius {} is:".format(r),area)
+area=PI*r*r
+print("The area of the circle of radius {} is:".format(r),area)
